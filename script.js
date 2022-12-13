560e47d59299b7925ceb8057c532901cb3be14b9
PeterKormos <110331177+PeterKormos0@users.noreply.github.com>
2022-12-13 21:03:35 +0100

    open and close modal added

--- script.js
+++ script.js
@@ -6,8 +6,19 @@ let btnCloseModal = document.querySelector('.close-modal');
 let btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    (btnsOpenModal[i].addEventListener('click', function () {
-        console.log("BUTTON CLICKED");
-    }));
-}
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
