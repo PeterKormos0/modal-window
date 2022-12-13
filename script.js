f887dd7dea9c852e8a7b9c61a516f37717903fbe
PeterKormos <110331177+PeterKormos0@users.noreply.github.com>
2022-12-13 15:52:49 +0100

    first lines added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overaly');
+let btnCloseModal = document.querySelector('.close-modal');
+let btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    console.log(btnsOpenModal[i].textContent);
+}
